fc01a0862d9d0074594a677ed34cd63360dcece2
MrWipp <114902448+MrWipp@users.noreply.github.com>
2023-03-28 20:50:48 +0300

    Update Port Scanner.py

--- Port Scanner/Port Scanner.py
+++ Port Scanner/Port Scanner.py
@@ -1,17 +1,32 @@
-from socket import *
-import time
-startTime = time.time()
+import socket
+import sys
+from datetime import datetime
 
-if __name__ == '__main__':
-   target = input('Enter the host to be scanned: ')
-   t_IP = gethostbyname(target)
-   print ('Starting scan on host: ', t_IP)
-   
-   for i in range(50, 500):
-      s = socket(AF_INET, SOCK_STREAM)
-      
-      conn = s.connect_ex((t_IP, i))
-      if(conn == 0) :
-         print ('Port %d: OPEN' % (i,))
-      s.close()
-print('Time taken:', time.time() - startTime)
+
+if len(sys.argv) == 2:
+    target = socket.gethostbyname(sys.argv[1]) # Translate hostname to IPV4
+else:
+    print("Invalid amount of args")
+    print("Syntax: python3 portscanner.py <ip>")
+
+try:
+    for port in range(50,85):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = s.connect_ex((target,port))
+        if result == 0:
+            print(f"Port {port} is open")
+        s.close()
+except KeyboardInterrupt:
+    print("\n Exiting Program..")
+    sys.exit()
+
+    
+except socket.gaierror:
+    print("Hostname could not be resolved.")
+    sys.exit()
+
+
+except socket.error:
+    print("Could not connect to the server.")
+    sys.exit()
